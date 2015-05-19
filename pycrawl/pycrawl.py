38256c38df2a54a3ace69431c3414853f7b8e2ff
karlorg <karlorg@users.noreply.github.com>
2015-05-19 11:31:24 +0100

    silly little robotparser example

--- pycrawl/pycrawl.py
+++ pycrawl/pycrawl.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+try:
+    from urllib import robotparser  # Python 3
+except ImportError:
+    import robotparser  # Python 2
+
+
+def main():
+    rp = robotparser.RobotFileParser("http://www.tvtropes.com/robots.txt")
+    rp.read()
+    url = "http://www.tvtropes.com/pmwiki/pmwiki.php/Main/AcmeProducts"
+    if rp.can_fetch("*", url):
+        print("we're good")
+    else:
+        print("uh oh")
+
+if __name__ == '__main__':
+    main()
