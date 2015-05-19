a2ffdf07ab1fa973045342ae663311ec72a609db
karlorg <karlorg@users.noreply.github.com>
2015-05-19 14:49:20 +0100

    refactor robots.txt checking

--- pycrawl/pycrawl.py
+++ pycrawl/pycrawl.py
@@ -25,16 +25,7 @@ def main(argv=None):
 
 
 def links_from_url(url):
-    parsed_url = urlparse(url)
-    robots_url_obj = parsed_url._replace(path='/robots.txt')
-    robots_url = robots_url_obj.geturl()
-    rp = robotparser.RobotFileParser(robots_url)
-    try:
-        # the following does not error on missing robots.txt files
-        rp.read()
-        if not rp.can_fetch("*", url):
-            return []
-    except:
+    if not can_robots_fetch(url):
         return []
     return links_from_data(requests.get(url).text)
 
@@ -45,6 +36,21 @@ def links_from_data(data):
     for tag in bs.find_all('a'):
         result.append(tag['href'])
     return result
+
+
+def can_robots_fetch(url):
+    parsed_url = urlparse(url)
+    robots_url_obj = parsed_url._replace(path='/robots.txt')
+    robots_url = robots_url_obj.geturl()
+    rp = robotparser.RobotFileParser(robots_url)
+    try:
+        # the following does not error on missing robots.txt files
+        rp.read()
+        if not rp.can_fetch("*", url):
+            return False
+    except:
+        return False
+    return True
 
 
 if __name__ == '__main__':
