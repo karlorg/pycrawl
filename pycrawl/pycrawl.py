0e9544e5ac34c7434a3b3b52d1c1e2085dd15907
karlorg <karlorg@users.noreply.github.com>
2015-05-19 13:11:46 +0100

    get links from given URL with HTMLParser

--- pycrawl/pycrawl.py
+++ pycrawl/pycrawl.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
-try:
-    from urllib import robotparser  # Python 3
+try:  # Python 3
+    from html.parser import HTMLParser
+    from urllib import robotparser
     from urllib.parse import urlparse
-except ImportError:
-    import robotparser  # Python 2
+except ImportError:  # Python 2
+    from HTMLParser import HTMLParser
+    import robotparser
     from urlparse import urlparse
+
+import requests
 
 
 def main(argv=None):
@@ -18,15 +22,42 @@ def main(argv=None):
     except IndexError:
         print("Usage: {} URL".format(sys.argv[0]))
         sys.exit(1)
+    print(links_from_url(url))
+
+
+class HTMLLinkFinder(HTMLParser):
+    def __init__(self, *args, **kwargs):
+        self.found_links = []
+        HTMLParser.__init__(self, *args, **kwargs)
+
+    def handle_starttag(self, tag, attrs):
+        if tag != 'a':
+            return
+        attr_dict = dict(attrs)
+        try:
+            self.found_links.append(attr_dict['href'])
+        except:
+            pass
+
+    def reset(self):
+        self.found_links = []
+        HTMLParser.reset(self)
+
+
+def links_from_url(url):
     parsed_url = urlparse(url)
     robots_url_obj = parsed_url._replace(path='/robots.txt')
     robots_url = robots_url_obj.geturl()
     rp = robotparser.RobotFileParser(robots_url)
     rp.read()
-    if rp.can_fetch("*", url):
-        print("we're good")
-    else:
-        print("uh oh")
+    if not rp.can_fetch("*", url):
+        return []
+    data = requests.get(url).text
+
+    link_finder = HTMLLinkFinder()
+    link_finder.feed(data)
+    return link_finder.found_links
+
 
 if __name__ == '__main__':
     main()
