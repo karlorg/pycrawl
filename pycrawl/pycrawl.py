0d4bcfcf1c6d9acf5b5dbc932945e00f2b444d05
karlorg <karlorg@users.noreply.github.com>
2015-05-19 14:41:17 +0100

    switch to BeautifulSoup for parsing

--- pycrawl/pycrawl.py
+++ pycrawl/pycrawl.py
@@ -3,14 +3,13 @@
 
 import sys
 try:  # Python 3
-    from html.parser import HTMLParser
     from urllib import robotparser
     from urllib.parse import urlparse
 except ImportError:  # Python 2
-    from HTMLParser import HTMLParser
     import robotparser
     from urlparse import urlparse
 
+import bs4
 import requests
 
 
@@ -23,25 +22,6 @@ def main(argv=None):
         print("Usage: {} URL".format(sys.argv[0]))
         sys.exit(1)
     print(links_from_url(url))
-
-
-class HTMLLinkFinder(HTMLParser):
-    def __init__(self, *args, **kwargs):
-        self.found_links = []
-        HTMLParser.__init__(self, *args, **kwargs)
-
-    def handle_starttag(self, tag, attrs):
-        if tag != 'a':
-            return
-        attr_dict = dict(attrs)
-        try:
-            self.found_links.append(attr_dict['href'])
-        except:
-            pass
-
-    def reset(self):
-        self.found_links = []
-        HTMLParser.reset(self)
 
 
 def links_from_url(url):
@@ -60,9 +40,11 @@ def links_from_url(url):
 
 
 def links_from_data(data):
-    link_finder = HTMLLinkFinder()
-    link_finder.feed(data)
-    return link_finder.found_links
+    bs = bs4.BeautifulSoup(data)
+    result = []
+    for tag in bs.find_all('a'):
+        result.append(tag['href'])
+    return result
 
 
 if __name__ == '__main__':
